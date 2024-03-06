0ebd0ad8abc92e8f40c0c58646250a3fb3c5f26b
Romy Phil <162441266+adelLyar@users.noreply.github.com>
2024-03-06 12:50:31 +0200

    Create main.js

---
+++ main.js
@@ -0,0 +1,26 @@
+if(typeof pos === "undefined"){
+			pos = this.table.options.addRowPos;
+		}
+		
+		if(pos === "pos"){
+			pos = true;
+		}
+		
+		if(pos === "bottom"){
+			pos = false;
+		}
+		
+		return pos;
+	}
+var row = data instanceof Row ? data : new Row(data || {}, this),
+		top = this.findAddRowPos(pos),
+		allIndex = -1,
+		activeIndex, chainResult;
+
+if(!index){
+			chainResult = this.chain("row-adding-position", [row, top], null, {index, top});
+			
+			index = chainResult.index;
+			top = chainResult.top;
+		}
+		
